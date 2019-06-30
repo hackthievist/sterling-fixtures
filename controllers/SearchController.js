c7f306bb5ef978e85b8c52ad97642b58dd9c1ec3
hackthievist <sobogunifeoluwa@gmail.com>
2019-06-30 20:56:05 +0100

    :building_construction: Modified /search endpoint

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -4,7 +4,48 @@ const ElasticService = require('../services/ElasticService');
 const Search = {
   async search(req, res) {
     try {
-      const results = await ElasticService.search(req.body.index, req.body.type, req.body.search);
+      const data = req.body;
+      const search = {
+        min_score: 0.2,
+        query: {
+          bool: {
+            should: [
+
+            ],
+          },
+        },
+      };
+
+      const slugSearch = {
+        match: {
+          slug: data.slug,
+        },
+      };
+
+      const nameSearch = {
+        match: {
+          name: data.name,
+        },
+      };
+
+      const fixtureSlugSearch = {
+        match: {
+          fixtureSlug: data.fixtureSlug,
+        },
+      };
+
+      if (data.type === 'fixture' && data.fixtureSlug) {
+        search.query.bool.should.push(fixtureSlugSearch);
+      }
+
+      if (data.type === 'team' && data.slug) {
+        search.query.bool.should.push(slugSearch);
+      }
+
+      if (data.type === 'team' && data.name) {
+        search.query.bool.should.push(nameSearch);
+      }
+      const results = await ElasticService.search(req.body.index, req.body.type, search);
       const searchResults = await results.hits.hits.map(each => each._source);
       return ResponseHelper.json(200, res, 'Results retrieved successfully', searchResults);
     } catch (err) {
